07c33621e65a14b7855b4036fc3cbbe9ece51acf
maltissimo <matteo.altissimo@elettra.eu>
2024-05-14 16:55:51 +0200

    Commit from Cleanroom - 20240514

--- Communication/MCG.py
+++ Communication/MCG.py
@@ -11,6 +11,7 @@ Author M. Altissimo c/o Elettra Sincrotrone Trieste SCpA
 
 GPASCII = "gpascii -2"  # this is needed to start the interpreter on the Pmac, i.e. to init the Pmac
 EOT = "\x04"  # End of transmission character, to close connection to the PMAC. the SSH stays open. Equivalent to pass CTRL+C
+ACK = "\x06"  # ACKNOWLEDGE character, see https://donsnotes.com/tech/charsets/ascii.html.
 delim = "\r\n"
 right = "STDIN Open for ASCII Input"
 class Pmac_Shell():
@@ -40,25 +41,26 @@ class Pmac_Shell():
         self.rawoutput = rawoutput # Initialize the output bytes buffer string as None
         self.textoutput = textoutput
 
-    def openssh(self, ssh = None, pmac_ip = None, username = None,pwd = None ):
+    def openssh(self ):
         """
            This functions connects to PMAC, setting the pmac_shell attribute to a shell of ssh class (ssh.invoke_shell())
            The ip is set to be the loopback interface by default, username and password are dummy values as well.
            The correct values must be passed to the method by the caller
 
            :return pmac_shell: a shell from paramiko, that can be used to send commands down.
+        , ssh = None, pmac_ip = None, username = None,password = None
 
-           """
-        ssh = self.ssh
-        pmac_ip = self.pmac_ip
-        username = self.username
-        pwd = self.password
+        self.ssh = ssh
+        self.pmac_ip = pmac_ip
+        self.username = username
+        self.password = password
+        """
         # Load SSH host keys.
-        ssh.load_system_host_keys()
+        self.ssh.load_system_host_keys()
         # Add SSH host key when missing.
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         # credentials = get_credentials()
-        ssh.connect(pmac_ip, username = username, password = pwd)
+        self.ssh.connect(self.pmac_ip, username = self.username, password = self.password)
         #print("Successfully connected to ", credentials[0])
         pmac_shell = self.ssh.invoke_shell()
         if pmac_shell.get_transport().is_active():
@@ -93,69 +95,68 @@ class Pmac_Shell():
             message = message + "\n"
 
         self.pmac_shell.send(message)
-    def listen(self, nbytes=512):
-        """
+    """
+    def listen(self):
+        
         This function returns a bytes output of whatever the shell passed as pararameter contains.
 
         :param shell_connection: a paramiko active shell object
         :param nbytes: number of bytes to be listened to, default is 512
         :return output: a bytes object
 
-        """
         self.rawoutput = None #set it to None every time the function is called.
         while self.pmac_shell.recv_ready():  # there is space and scope here to add a timeout
             if self.pmac_shell.recv_ready():  # must test for recv_ready() in order for the code not to hang!
-                self.rawoutput = self.pmac_shell.recv(nbytes).decode("UTF-8")
+                self.rawoutput = self.pmac_shell.recv(self.nbytes).decode("UTF-8")
     def simple_output(self ):
-        """
+        
         This function produces a single string of output after a given command is issued. It is a combination of the
         listen method with the format_output and receive_message methods.
 
 
-        :param nbytes: set to 512 by default.
+        :param nbytes: set to 1024 by default.
         :return: a string, which should be usable for processing.
-        """
+        
         self.rawoutput = None
         while self.pmac_shell.recv_ready():  # there is space and scope here to add a timeout
             if self.pmac_shell.recv_ready():  # must test for recv_ready() in order for the code not to hang!
                 self.rawoutput = self.pmac_shell.recv(self.nbytes).decode("UTF-8")
         lines = self.rawoutput.split(delim)
         return(str(lines[1]))
+        
     def format_output(self):
-        """
+        
         Formats the output of a shell.recv()command.
         :param bytes_buffer: output from the function listen
 
-        """
+        
         lines = self.rawoutput.split(delim)
         if not self.rawoutput.endswith(delim):
             self.rawoutput = lines[-1]
             lines = lines[:-1]
         for line in lines:
             yield line.rstrip()
+            """
     def receive_message(self):
         """
-        Prints the bytes of output obtained from the listen(shell) function.
-        The usage should be as follows:
-        shell.send("mycommand\n")
-        out = listen(shell)
-        printout(out)
+       sets the textouput property of self as an array of strings.
+       Depending on the command before, the relevant output is
+       self.textoutput[-2] after a pmac_init
+       self.textoutput[-3] after any other command.
 
         :return:
         """
-        if self.rawoutput is not None:  #checking that the attribute has been assigned seems sane.
-            self.textoutput = []
-            if self.pmac_shell.recv_ready():
-                self.rawoutput = self.pmac_shell.recv(nbytes).decode("ascii")
-                lines = self.rawoutput.split(delim)
-            self.textoutput.extend(lines)
-
+        self.textoutput = []
+        if self.pmac_shell.recv_ready():
+            self.rawoutput = self.pmac_shell.recv(self.nbytes).decode("ascii")
+            outlines= self.rawoutput.split(delim)
+            self.textoutput.extend(outlines)
         else:
-            self.textoutput = "Something went wrong with the connection"
+            self.textoutput = ("Something went wrong with the connection")
 
         #print(self.textoutput)
     def is_open(self):
-        """
+        """s
        Checks if the connection is alive or not
        Uses the get_transport() function to gain access to the Transport members in paramiko, returning True if the connection is active
         This throws an error if the connection is not active, so careful, hence the method
@@ -189,11 +190,10 @@ class Gantry(Pmac_Shell):
         if self.alive is not False:
             self.send_message(message) # this sends the message down to the SSH connection
             time.sleep(0.02)
-            while self.pmac_shell.recv_ready():  # there is space and scope here to add a timeout
-                if self.pmac_shell.recv_ready():  # must test for recv_ready() in order for the code not to hang!
-                    self.rawoutput = self.pmac_shell.recv(self.nbytes).decode("UTF-8")
-            lines = self.rawoutput.split(delim)
-            return (str(lines[1]))
+             # there is space and scope here to add a timeout
+            self.receive_message()
+
+            return (str(self.textoutput[-3]))
 
             """output = self.simple_output()
             return(output)"""
@@ -211,39 +211,15 @@ class Gantry(Pmac_Shell):
         response = self.simple_output()
 
         """
+        self.send_message("\n")
         self.send_message(GPASCII)
-        response = self.simple_output()
+        self.receive_message()
+        response = self.textoutput[-2]
         if response == right:
             self.isinit = True
             return(response)
         else:
             return(response)
-        """self.rawoutput = None
-        while self.pmac_shell.recv_ready():  # there is space and scope here to add a timeout
-            if self.pmac_shell.recv_ready():  # must test for recv_ready() in order for the code not to hang!
-                self.rawoutput = self.pmac_shell.recv(self.nbytes).decode("ascii")
-                self.rawoutput += self.pmac_shell.recv(self.nbytes).decode("ascii")
-                self.rawoutput += self.pmac_shell.recv(self.nbytes).decode("ascii")
-        lines = self.rawoutput.split(delim)
-        response = lines
-        #print(response)
-        if response[-1] == right:
-            self.isinit = True  # Pmac is now ready for accepting input
-            return (response)
-
-        
-        response[-1] != right:
-            #raise ValueError("Wrong output")
-            print(response)
-            self.isinit = False
-            return(response)
-        elif response[-1] == right:
-            """
-
-    def pmac_close(self):
-        self.send_message(EOT)
-        self.isinit = False
-
     def set_echo(self):
         """
         this function checks on the PMAC if echo is on or off. If echo is on, then:
