762d2baab1e20018929544c5229064cfce6b20ef
maltissimo <matteo.altissimo@elettra.eu>
2024-06-03 12:26:24 +0200

    Commit of the LTPy project, June 3rd 2024

--- Hardware/laser.py
+++ Hardware/laser.py
@@ -4,5 +4,5 @@ from MCL import *
 
 class Laser (SerialConn):
     def __init__(self, comms_on = False, is_on = False, wlength = 0.0, pow_level = 0.0, cur_level = 0.0):
-        self.comms = self.serialmessage(oh.isLASON)
+        self.comms = self.serialmessoige(oh.isLASON)
 
