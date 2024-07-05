c2d93b38407b88341db52b40355ab50a74ceba5d
Matteo Altissimo <matteo.altissimo@elettra.eu>
2024-07-05 12:00:52 +0200

    Commit of the LTPy project 05 July 2024
    Measurement class now with proper __init__, measure and save_data methods.
    
    Modified structure of the project in order to make it more general.
    Started implementing the Control Center, including a Control_utilities.py module for helper functions
    started implementation on Measurement.py
    
    Modified Motors module,
    de-bugged the Motor Class, added CompMotor, MotorUtil and Move classes, removed Initer (method moved inside MotorUtil).
    Added some exception handling
    Updated classtests.py for module testing.

--- ControlCenter/Measurement.py
+++ ControlCenter/Measurement.py
@@ -2,7 +2,7 @@ import datetime
 from Hardware import Source, Motors, Detector
 from Communication import MCG, MCL
 from ControlCenter import Control_Utilities as cu
-import scipy
+from scipy import ndimage
 
 TODAY = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
@@ -33,14 +33,15 @@ camera.opencam()
 # Now the fun begins:
 
 class Measurement():
-    def __init__(self, nr_of_points = 10, length = 1, \
-                 xmotor, xmove,\
-                 ymotor, ymove,  \
-                 zmotor, zmove, \
-                 pitch, pitchmove,  \
-                 roll, rollmove,  \
-                 yaw, yawmove, \
+    def __init__(self, nr_of_points = 10, length = 1,
+                 xmotor, xmove,
+                 ymotor, ymove,
+                 zmotor, zmove,
+                 pitch, pitchmove,
+                 roll, rollmove,
+                 yaw, yawmove,
                  camera):
+
         self.nr_of_points = nr_of_points
         self.length = length  # length of measurement, units in m. Must be converted to µm, as that's the unit of X.
         self.xmotor = xmotor
@@ -66,17 +67,57 @@ class Measurement():
         self.roll_start_post = self.roll.get_real_pos()
         self.yaw_start_pos = self.yaw.get_real_pos()
         self.images = []
-        self.sampling = self.length / self.nr_of_points
+        self.step = self.length / self.nr_of_points
+        self.results = ""
 
     def measure(self):
         """
         this assumes the X stage is already in its correct starting position
         :return:
         """
+        # preparing the results header
+
+        self.results += "date of measurement: " + TODAY + "\n"
+        self.results += "\t" + "X" + "\t" + "Y" + "\t" + "Z" + "\t" + "P" + "\t" + "R" + "\t" + "Y"+ "\t" + "Centroid" + "\n"
+        self.results += "\n"
+
         for i in range(self.nr_of_points):
             image = self.camera.grabdata()  # already grabbing 10 images by default
+            step_result = self.centroid(image) #this is an array, with 2 coordinates of the centroid.
+            #Result for this position:
+            self.results += self.get_all_motor_pos()
+            self.results += step_result
+            self.xmove.move_rel(self.step)
+    def centroid(self, ndarray):
+        centroid = ndimage.center_of_mass(ndarray)
+        return(centroid)
 
-            #TODO: finish writing the measure method. Check camera_tests.py et similia 20240704
+    def get_all_motor_pos(self):
+        """
+        This spits out a formatted text. I think it's better creating a method rather than using inside the measure for cycle.
+        :return: all_motor_pos.
+        """
+        pos_all = ""
+        pos_all += str(self.xmotor.get_real_pos) + "\t"
+        pos_all += str(self.ymotor.get_real_pos) + "\t"
+        pos_all += str(self.zmotor.get_real_pos) + "\t"
+        pos_all += str(self.pitch.get_real_pos) + "\t"
+        pos_all += str(self.roll.get_real_pos) + "\t"
+        pos_all += str(self.yaw.get_real_pos) + + "\t"
+        return(pos_all)
+
+    def save_data(self):
+        filename = "data of " + TODAY ".txt"
+        with open (filename, "w", encoding = "ASCII") as f:
+            f.write(self.results)
+
+        print("Data saved into: " + filename )
+
+
+
+#TODO: start working on a GUI. See Dynamic Graph in the Gantrycomms directory for starters
+#TODO: implement a checking method in Control Utilities that works before each measurement is performed
+# and checks whether all the subsystems are operational.
 
 
 
